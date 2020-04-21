ba31e2ebabc0cd9074f1d5b9f65578959a7c6942
Kayode <oluwaseyikayode25@gmail.com>
2020-04-21 12:22:15 +0100

    ksnaija

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, get_object_or_404
 from django.contrib.auth.models import User
 from django.db.models import Q
+from django.template import RequestContext
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
 from django.views.generic.edit import FormMixin
@@ -106,7 +107,16 @@ class SearchView(ListView):
         return object_list
 
 
+def handler404(request, exception, template_name="404.html"):
+    response = render(request, template_name)
+    response.status_code = 404
+    return response
 
+
+def handler500(request, exception, template_name="500.html"):
+    response = render(request, template_name)
+    response.status_code = 500
+    return response
 
 
 
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import handler404, handler500
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -27,3 +28,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'blog.views.handler404'
+handler500 = 'blog.views.handler500'
