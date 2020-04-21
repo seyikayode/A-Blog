21f1a0825c4cb2dfa5ea6465bc8215b59e1b96ec
Kayode <oluwaseyikayode25@gmail.com>
2020-04-21 08:31:41 +0100

    ksnaija

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG_VALUE', ''))
 
 ALLOWED_HOSTS = ['ksnaija.herokuapp.com']
 
