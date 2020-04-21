fda4b6eaff32eb2f078fbf0c5ae72a66bb276630
Kayode <oluwaseyikayode25@gmail.com>
2020-04-21 03:38:38 +0100

    ksnaija

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_root')
 ]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
