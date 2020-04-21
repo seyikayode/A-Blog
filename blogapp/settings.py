91289000be4571b3ab4a82d838793bc43fb515b8
Kayode <oluwaseyikayode25@gmail.com>
2020-04-21 03:45:26 +0100

    ksnaija

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -129,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_root')
 ]
